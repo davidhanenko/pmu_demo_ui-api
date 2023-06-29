57af4c45290f17fc399dc1cc38ee18fae6c21b21
davidhanenko <d.hanenko24@gmail.com>
2023-06-28 21:23:10 -0400

    resolve video player issue

--- src/components/brows/Brows.tsx
+++ src/components/brows/Brows.tsx
@@ -7,9 +7,12 @@ import { BrowsTypes } from './BrowsTypes';
 import { BrowsSteps } from './BrowsSteps';
 import { SectionHeader } from '../shared';
 
-import browImg from '../../assets/images/brow_1.png';
-import { CldVideoPlayer } from 'next-cloudinary';
+import browImg from '@/assets/images/brow_1.png';
+
 import { BrowsDescription } from './BrowsDescription';
+
+const videoSrc =
+  'https://res.cloudinary.com/ddqehibx0/video/upload/v1684093291/pmu/brows_v_1_isrehe.mp4';
 
 export const Brows = () => {
   const { ref, inView } = useInView({
@@ -36,17 +39,16 @@ export const Brows = () => {
         </div>
         <div className='relative'>
           <div className='relative md:clip-bg-brows-2 lg:clip-bg-brows-1 h-full w-full'>
-            <CldVideoPlayer
-              className=''
-              width={500}
-              height={250}
-              autoPlay='on-scroll'
-              quality='auto'
-              loop={true}
-              muted={true}
-              controls={false}
-              src='pmu/brows_v_1_isrehe'
-            />
+            <video
+              width='1400'
+              height='720'
+              muted
+              loop
+              autoPlay
+              className='h-full w-full'
+            >
+              <source src={videoSrc} type='video/mp4' />
+            </video>
             <div className='absolute top-0 left-0 h-full w-full bg-[#000] opacity-70' />
 
             <BrowsDescription />
